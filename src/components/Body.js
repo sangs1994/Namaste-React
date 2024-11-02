f1528f8cfd68559e5022e12cd301d6a2af1706e4
sangs1994 <sangeetha94.chandrasekar@gmail.com>
2024-11-02 21:04:14 +0530

    Top Rating Filter Feature Implemented

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,22 @@
 import RestCard from "./RestCard";
+import { useState } from "react";
 import swiggy_data from "../utils/swiggy_data";
 
 const Body = () => {
-  const resList = swiggy_data;
+  const [resList, setResList] = useState(swiggy_data);
   console.log(resList);
   return (
     <div className="body">
       <div className="search">Search</div>
+      <button
+        className="top-rated-button"
+        onClick={() => {
+          const filteredItem = resList.filter((res) => res.info.avgRating > 4);
+          setResList(filteredItem);
+        }}
+      >
+        Top Rated Button
+      </button>
       <div className="res-container">
         {resList.map((res) => (
           <RestCard key={res.id} resData={res}></RestCard>
