ed5356ff941e124b6dde6bd4560fc0e1868c91d8
sangs1994 <sangeetha94.chandrasekar@gmail.com>
2024-11-13 20:29:06 +0530

    varaible changes

--- src/components/RestCard.js
+++ src/components/RestCard.js
@@ -1,16 +1,23 @@
 import { CDN_URL } from "../utils/data";
 
 const RestCard = (props) => {
-  const { name, cuisines, avgRatingString, sla, cloudinaryImageId } =
-    props.resData.info;
+  const {
+    name,
+    cuisines,
+    avgRatingString,
+    sla,
+    cloudinaryImageId,
+    costForTwo,
+  } = props.resData.info;
 
   return (
     <div className="res-card">
       <img className="res-card-image" src={CDN_URL + cloudinaryImageId}></img>
       <h3 className="res-card-name">{name}</h3>
       <h4 className="res-card-cuisine">{cuisines.join(", ")}</h4>
-      <h4 className="res-card-rating">{avgRatingString}</h4>
-      <h4 className="res-card-eta">{sla.deliveryTime}</h4>
+      <h4 className="res-card-rating">{avgRatingString} stars</h4>
+      <h4 className="res-card-eta">{sla.deliveryTime} minutes</h4>
+      <h4 className="res-card-cost">{costForTwo}</h4>
     </div>
   );
 };
