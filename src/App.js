ec6c07106bd1f00768f1dd10ed9f8ed30abb4716
sangs1994 <sangeetha94.chandrasekar@gmail.com>
2024-11-14 18:14:02 +0530

    React Router DOM implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 /*****
  * <h1> Hello World from React </h1>
@@ -61,5 +65,11 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  { path: "/", element: <AppLayout />, errorElement: <Error /> },
+  { path: "/about", element: <About />, errorElement: <Error /> },
+  { path: "/contact", element: <Contact />, errorElement: <Error /> },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
